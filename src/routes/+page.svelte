678f41f37cb4c1c6e69af59774d97b5841e5a32c
benzerer <b.intm@hotmail.com>
2026-02-17 12:49:34 +0700

    feat: remove onMount import and adjust image size and main container height

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import RandomImage from '../lib/RandomImage.svelte';
 </script>
 
-<main class="flex flex-col items-center justify-center min-h-screen p-8 bg-linear-to-b from-white to-orange-100">
+<main class="flex flex-col items-center justify-center min-h-svh p-8 bg-linear-to-b from-white to-orange-100">
   <header class="text-center">
     <h1 class="text-2xl font-bold text-orange-600">ปีนี้คุณเป็นม้าอะไร</h1>
   </header>
