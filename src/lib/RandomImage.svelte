d43833a100f66de718e69ac31f9ad430f807f8e4
benzerer <b.intm@hotmail.com>
2026-02-17 12:52:43 +0700

    feat: adjust image size in RandomImage component for better responsiveness

--- src/lib/RandomImage.svelte
+++ src/lib/RandomImage.svelte
@@ -15,6 +15,6 @@
 <div class="flex flex-col items-center justify-center gap-6 p-4 w-full">
   <button on:click={handleRandomize} class="bg-orange-500 hover:bg-orange-600 text-white font-bold py-3 px-8 rounded-full transition-transform hover:scale-110">สุ่มเลยยย!!</button>
   {#if imageSrc}
-    <img src={imageSrc} alt="Random result" class="max-w-full rounded-2xl shadow-lg" />
+    <img src={imageSrc} alt="Random result" class="max-w-[280px] rounded-2xl shadow-lg" />
   {/if}
 </div>
